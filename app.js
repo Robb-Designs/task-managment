def57353db05727755b5ebac3e293d02e2249305
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-20 15:17:52 -0400

    set up the toggle for the moddal to add task, cancel to close modal

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const addTaskBtn = document.getElementById("add-btn");
+const modal = document.getElementById("task-modal");
+//modal caching
+const addBtnModal = document.getElementById("add-btn-modal");
+const cancelBtnModal = document.getElementById("cancel-btn-modal");
+
+
+
+//click to have modal visible for input
+addTaskBtn.addEventListener("click", ()=>{
+    //removes hidden class on the modddal's ID when clicke
+    modal.classList.remove("hidden");
+})
+
+//cancel button to close the modal
+cancelBtnModal.addEventListener("click", ()=>{
+    modal.classList.add("hidden")
+})
+
+
+
