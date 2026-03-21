9a91a50c8e9930056c1a84e5f9abf6c94fe9ed8d
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-21 00:51:11 -0400

    Refactor/add task logic: map status to container and append task dynamically

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ let statusInput = document.getElementById("status");
 let deadlineInput = document.getElementById("deadline-date");
 let descriptionInput = document.getElementById("form-description");
 
+// made a map obj...if the user selects a status, then the div gets appended to the corresponding container map property
+// better to map like this insteadd of using a for loop andd condditionals, this will scale easier
+const containerMap = {
+    "to-do": document.getElementById("to-do-content"),
+    "in-progress": document.getElementById("in-progress-content"),
+    "completed": document.getElementById("completed-content"),
+    "overdue": document.getElementById("overdue-content")
+};
+
 
 
 
@@ -36,19 +45,32 @@ let createTaskObj = () => {
         category: categoryInput.value,
         deadline: deadlineInput.value,
         description: descriptionInput.value,
-        status: "to-do" //default
+        status: statusInput.value
     };
     return inputToObj;
 }
 
 
 //
- addBtnModal.addEventListener("click", ()=>{
+addBtnModal.addEventListener("click", () => {
     const task = createTaskObj();
+    const targetContainer = containerMap[task.status];
+    // Caching the div that the task cards are being placed
+    const taskCard = document.createElement("div"); // created the div
+    taskCard.textContent = task.name; // setting text content of the div to be the task name from the task obj
+    targetContainer.appendChild(taskCard); //appending to the container
+
+    if (!targetContainer) {
+    console.error("Invalid status:", task.status);
+    return;
+}
+
     modal.classList.add("hidden"); //toggle the class to hidden
-    console.log(task);
-    
- })
+
+    console.log(`Task Status: ${task.status}`);
+    console.log(targetContainer);
+
+})
 
 
 
